7af0e3908c9bfceea18e3ce4e30a4b1b8c610aa7
1JUNQ <hanjk13262@gmail.com>
2021-11-02 11:47:31 +0900

    로그인 기능 완료

---
+++ src/main/webapp/Doc/js/Auth/main.js
@@ -0,0 +1,21 @@
+// window.onload = auth()
+// console.log(sessionStorage.getItem("accessToken"));
+const login_btn = document.querySelector(".btn.header-btn");
+if (sessionStorage.getItem("accessToken")) {
+  login_btn.innerText = "LOGOUT";
+  login_btn.addEventListener("click", () => {
+    switch (login_btn.innerText) {
+      case "LOGOUT":
+        sessionStorage.removeItem("accessToken");
+        login_btn.innerText = "LOGIN";
+        break;
+      case "LOGIN":
+        window.open("http://127.0.0.1:5501/src/main/webapp/login.html");
+        break;
+    }
+  });
+} else {
+  login_btn.addEventListener("click", () => {
+    window.open("http://127.0.0.1:5501/src/main/webapp/login.html");
+  });
+}
